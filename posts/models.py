c755c4d3b85d51b44fd5abf8a3af9c2f0c604e8f
AhmedRaafat3 <ahmedmostafarafat22@gmail.com>
2023-10-01 01:01:20 +0300

    design post model pt1

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+'''
+title
+author
+content
+image
+tags
+puplish date
+
+'''
+class post (models.Model):
+    title=models.CharField(max_length=50)
+    content=models.TextField(max_length=50000)
+    publish_date=models.DateTimeField()
+
